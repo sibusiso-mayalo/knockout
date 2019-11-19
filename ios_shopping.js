732bd343ed8b90c56e69a96a7ecd941b01811857
Sibusiso Mayalo <mylsbu001@myuct.ac.za>
2019-11-20 00:21:13 +0200

    Refactored code and added comments inside functions

--- ios_shopping.js
+++ ios_shopping.js
@@ -16,17 +16,19 @@ function send_data(address, data)
 
 function calculate_total(array)
 {
+  //return the sum of all the prices on the given array (product catalog)
   var total = 0;
   array.forEach( function(item)
-    {
+  {
     var data = JSON.parse( ko.toJSON(item) );
     total += parseFloat(data["price"]);
   });
   return total;
 }
 
-  var viewModel = function()
-  {
+var viewModel = function()
+{
+  //declare all the variables
     this.self = this;
     this.id = ko.observable();
     this.name = ko.observable();
@@ -34,29 +36,23 @@ function calculate_total(array)
     this.price = ko.observable();
     this.items = ko.observableArray();
 
-    //called when client clicks one of the delete buttons
     self.delete = function(data)
     {
-      this.items().remove(data);
-      alert("Clicked");
+      //called when client clicks one of the delete buttons
+      this.items.remove(data);
     };
 
-    //called when client wish to add new info on the table
     self.add = function()
     {
+      //called when client wish to add new info on the table
       var order_item_dict = {id:this.id(), name:this.name(), code:this.code(), price:this.price()};
       this.items.push(order_item_dict);
     }
 
-    self.total = ko.pureComputed(function(){
-      return this.subtotal() * (1 + this.vat());
-    });
-
-  //called when the client wants to checkout
-  //NB: On the spec, this is described as sending client data as JSON to google and to chris's emial
-
   self.checkout = function()
   {
+    //called when the client wants to checkout
+    //NB: On the spec, this is described as sending client data as JSON to google and to chris's emial
     var total = calculate_total(this.items());
     var final_dict = {"total":total, "data":this.items()};
     final_dict = ko.toJSON( final_dict) ;//change the dict object to a string of json format [tot:total, data:data]
@@ -65,6 +61,5 @@ function calculate_total(array)
     //send_data("chrisg@iosystemssa.com", final_dict);
     //send_data('mayalo.sbusiso@icloud.com', final_dict);
   };
-
 };
 window.onload = function(){ ko.applyBindings(new viewModel()); };
